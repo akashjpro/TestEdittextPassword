e137f45409ffed3651061d80a4256ab3af7b4568
akashjpro <akashjpro@gmail.com>
2017-08-11 00:55:29 +0700

    Initial commit

---
+++ app/src/main/java/com/adida/aka/testedittextpassword/MainActivity.java
@@ -0,0 +1,34 @@
+package com.adida.aka.testedittextpassword;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imgShowAndHidePass;
+    private EditText tvPassword;
+    private boolean isShowPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgShowAndHidePass = (ImageView) findViewById(R.id.imgShowAndhidePass);
+        tvPassword = (EditText) findViewById(R.id.tvPassword);
+        imgShowAndHidePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isShowPass){
+                    isShowPass = true;
+                    tvPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }else {
+                    isShowPass = false;
+                    tvPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+            }
+        });
+    }
+}
